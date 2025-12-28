16830dd059c399bf2dd016045513fdefbc68cfb3
Jawadsandouk <105123117+Jawadsandouk@users.noreply.github.com>
2025-12-28 20:57:29 +0300

    Add Category model and migration

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name']; // Allow mass assignment for 'name'
+
+    // One-to-Many relationship: A category can have many books
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
